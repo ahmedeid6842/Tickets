76d6866df1e7e41835d6de698facdb2097fd53da
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-21 08:07:02 +0300

    Added morgan middleware for 'dev'

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,6 +2,7 @@ import "express-async-errors";
 import express from "express";
 import cookiePrser from "cookie-parser";
 import cors from "cors";
+import morgan from "morgan";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
@@ -15,6 +16,8 @@ app.use(
     origin: "*",
   })
 );
+
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookiePrser());
 
