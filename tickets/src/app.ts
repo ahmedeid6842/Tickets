d1c9bcf6645d7bcd74253fbb18f0530439ae3f46
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-22 06:46:47 +0300

    Added env checks & connect to DB using env

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,16 @@
 import "express-async-errors";
 import express from "express";
 import morgan from "morgan";
+import cors from'cors';
 
 const app = express();
 
 app.set("trust proxy", true);
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 import { app } from "./app";
 const start = async () => {
-  // if (!process.env.JWT_KEY) {
-  //   throw new Error("JWT_KEY must be defined");
-  // }
+  if (!process.env.JWT_KEY || !process.env.SALT_FACTORY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.SALT_FACTORY) {
+    throw new Error("SALT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log("connected to mongodb 🚀 🎯");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDb 🎯");
   } catch (err) {
     console.error(err);
+    return;
   }
 
-  app.listen(3000, () => console.log("connected on port 3000 🕵️‍♂️"));
+  app.listen(3000, () => console.log("Tickets: connected on port 3000 🕵️‍♂️"));
 };
 start();
