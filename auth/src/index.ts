82e1462dac5ce1458339742326ea1b6116477030
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-22 07:40:10 +0300

    Added mongoose schema for ticket

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,14 @@ const start = async () => {
   if (!process.env.JWT_KEY || !process.env.SALT_FACTORY) {
     throw new Error("JWT_KEY must be defined");
   }
-
+  if (!process.env.SALT_FACTORY) {
+    throw new Error("SALT_KEY must be defined");
+  }
+  if(!process.env.MONGO_URI){
+    throw new Error("MONGO_URI must be defined")
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb 🎯");
   } catch (err) {
     console.error(err);
@@ -15,7 +20,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("Listening on port 3000 🚀");
+    console.log("Auth: Listening on port 3000 🚀");
   });
 };
 
---
+++ tickets/src/models/tickets.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDocs extends mongoose.Document {
+  title: number;
+  price: number;
+  userId: string;
+}
+
+interface TicketModal extends mongoose.Model<TicketDocs> {
+  build(attr: TicketAttrs): TicketDocs;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, returnedDoc) {
+        returnedDoc.id = returnedDoc._id;
+        delete returnedDoc._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDocs, TicketModal>("Tickets", ticketSchema);
