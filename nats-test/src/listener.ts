a0beac7a015bfa10b341df3fa4bd17a43c7b9a32
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-23 11:44:23 +0300

    Added NATS Queue group to run to multiple instance with unique ID for each instance

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,22 +1,24 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subsciption = stan.subscribe("ticket:created");
+  const subsciption = stan.subscribe("ticket:created", "listern-queue-group");
 
   subsciption.on("message", (message: Message) => {
     const data = message.getData();
 
     if (typeof data == "string") {
-      console.log(`Received Event #${message.getSequence()}, with data: ${data}`);
+      console.log(
+        `Received Event #${message.getSequence()}, with data: ${data}`
+      );
     }
   });
 });
-
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,8 +1,9 @@
 import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "abc", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
