e9b2613be16e033e1b9e29f33c344bd7e2e69eb4
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-23 10:40:58 +0300

    setup publisher for nats-test with nats service

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,19 @@
+import nats from "node-nats-streaming";
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS service");
+  const message = JSON.stringify({
+    id: "123",
+    title: "concert",
+    price: 20,
+  });
+
+  stan.publish("ticket:created", message, () => {
+    console.log("Event published");
+  });
+});
+
