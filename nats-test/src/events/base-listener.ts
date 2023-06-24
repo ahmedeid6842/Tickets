f02d16252bdc201697ef1289d14603a7c6c8d02a
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-24 09:01:13 +0300

    Added nats listener validation

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,9 +1,15 @@
 import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
-export abstract class Listener {
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+
+export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   protected ackWait = 5 * 1000; // 5 seconds
 
   constructor(private cilent: Stan) {}
--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,12 +1,19 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+
   queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message): void {
-    console.log(`Event data! ${data}`);
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
+    console.log(`Event data! ${JSON.stringify(data)}`);
+
+    console.log(data.id);
+    console.log(data.title);
+    console.log(data.price);
 
     msg.ack();
   }
