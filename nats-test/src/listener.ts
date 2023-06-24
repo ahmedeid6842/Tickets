5a7e7e897663da6f5ebbc49b4f44af0dbddc4955
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-24 08:11:49 +0300

    Refactored nats listeners

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -15,28 +16,7 @@ stan.on("connect", () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName("accounting-srv");
-
-  const subsciption = stan.subscribe(
-    "ticket:created",
-    "listener-queue-group",
-    options
-  );
-
-  subsciption.on("message", (message: Message) => {
-    const data = message.getData();
-
-    if (typeof data == "string") {
-      console.log(
-        `Received Event #${message.getSequence()}, with data: ${data}`
-      );
-    }
-    message.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on("SIGINT", () => stan.close());
