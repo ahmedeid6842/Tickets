60eec550164ed84f4362b74361501f6dc102e985
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-08 10:42:41 +0300

    Setup the routes into entry point [index.ts]

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,7 +3,11 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import cookiePrser from "cookie-parser";
-
+import { currentUser } from "./middleware/currentUser";
+import { deleteOrderRouter } from "./routes/delete";
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
 
 const app = express();
 
@@ -17,5 +21,12 @@ app.use(
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookiePrser());
+
+app.use(currentUser);
+
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 export { app };
