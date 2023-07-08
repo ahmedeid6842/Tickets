b3a8bf41d3029d19b4b39948e16400e889d7ba73
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-08 10:33:08 +0300

    Added the create order handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,39 @@
 import express, { Request, Response } from "express";
 import { requireAuth } from "../middleware/requireAuth";
+import { Ticket } from "../models/ticket";
+import { Order, OrderStatus } from "../models/order";
 
 const router = express.Router();
 
 router.post("/api/orders", requireAuth, async (req: Request, res: Response) => {
-  res.send({});
+  const { ticketId } = req.body;
+
+  // check if the ticket is already exist or not
+  const ticket = await Ticket.findById(ticketId);
+  if (!ticket) {
+    return res.status(404).send("No Ticket found");
+  }
+
+  // check if the ticket is already reserved 
+  const isReserved = await ticket.isReserved();
+  if (isReserved) {
+    return res.status(400).send("Ticket is already reserved");
+  }
+
+  // set the expiration date for the ticket
+  const expiration = new Date();
+  expiration.setSeconds(expiration.getSeconds() + 15 * 60);
+
+  // Build the ticket order 
+  const order = Order.build({
+    userId: req.currentUser!.id,
+    status: OrderStatus.Created,
+    expiresAt: expiration,
+    ticket,
+  });
+  await order.save();
+
+  res.status(201).send(order);
 });
 
 export { router as newOrderRouter };
