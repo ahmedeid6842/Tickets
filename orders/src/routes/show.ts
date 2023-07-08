221ee39b11de14e297a1606a85919979cb558403
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-08 10:38:01 +0300

    Added fetching order data handler [show,index]

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,27 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
+import { Order } from "../models/order";
+import { requireAuth } from "../middleware/requireAuth";
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.get(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order) {
+      return res.status(404).send({ message: "No order found" });
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      return res
+        .status(403)
+        .send({ message: "un authorized to edit this ticket" });
+    }
+
+    res.send(order);
+  }
+);
 
 export { router as showOrderRouter };
