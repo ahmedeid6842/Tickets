09e93f10875d9ff8e0f84a227cbf77e14059e9f1
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-09 10:25:40 +0300

    Resolving events' concurrency issue : Ticket service 	modified:   common/events/event-interfaces/ticket-created-event.ts 	modified:   common/events/event-interfaces/ticket-updated-event.ts         Add version to event interface so the incoming events should include the version number 	modified:   tickets/src/models/tickets.ts         - Used mongoose-update-if-current package to handle the increment of version number         - Added the version to ticket Docs 	modified:   tickets/src/routes/newTicket.ts         - Integrate the version to the publish ticket create event 	modified:   tickets/src/routes/updateTicket.ts         - Integrate the version to the publish ticket update event

--- tickets/src/routes/updateTicket.ts
+++ tickets/src/routes/updateTicket.ts
@@ -28,6 +28,7 @@ router.put("/api/tickets/:id", async (req: Request, res: Response) => {
     title: ticket.title,
     price: ticket.price,
     userId: ticket.userId,
+    version: ticket.version,
   });
   res.send(ticket);
 });
