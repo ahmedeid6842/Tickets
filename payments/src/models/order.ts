3d7162d7edb6e65c9dad33e89f788b07e46c69a7
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-11 09:01:07 +0300

    Added the order database model of the payments service

---
+++ payments/src/models/order.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { OrderStatus } from "@aetickets/common";
+
+interface OrderAttrs {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  price: number;
+  status: number;
+  version: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret.id;
+      },
+    },
+  }
+);
+
+orderSchema.set("versionKey", "version");
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    userId: attrs.userId,
+    price: attrs.price,
+    status: attrs.status,
+    version: attrs.version,
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
