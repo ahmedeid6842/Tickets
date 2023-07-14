63341f171b0daf1de8e41eee567d02f4541269ca
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-07-14 22:35:26 +0300

    Removed unnecessary nats test directory

--- nats-test/src/events/ticket-created-publisher.ts
+++
@@ -1,7 +0,0 @@
-import { Publisher } from "./base-publisher";
-import { Subjects } from "./subjects";
-import { TicketCreatedEvent } from "./ticket-created-event";
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  readonly subject = Subjects.TicketCreated;
-}
